3d79dee69ed0cd101a836f31b724bb7ca17e76f6
AenimaLabs <aenimalabs@gmail.com>
2021-02-16 10:17:34 -0300

    remoto y binding en MainActivity

--- app/src/main/java/com/example/segundapruebajorgeandaurrios/data/Remote.kt
+++ app/src/main/java/com/example/segundapruebajorgeandaurrios/data/Remote.kt
@@ -1,2 +1,33 @@
 package com.example.segundapruebajorgeandaurrios.data
 
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+//https://my-json-server.typicode.com/himuravidal/gamesDB/games
+//https://my-json-server.typicode.com/himuravidal/gamesDB/gameDetails/4200
+
+interface GamesAPI {
+    @GET("games")
+    suspend fun getGames(): Response<List<Games>>
+
+    @GET("gamesDetails/{id}")
+    suspend fun getGames (@Path("id") id:String): Response<GamesDetail>
+}
+
+class RetrofitClient {
+    companion object {
+        private const val BASE_URL = "https://my-json-server.typicode.com/himuravidal/gamesDB/"
+
+        fun retrofitInstance(): GamesAPI {
+            val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(
+                GsonConverterFactory.create())
+                .build()
+
+            return retrofit.create(GamesAPI::class.java)
+        }
+    }
+}
